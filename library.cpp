edc8ad4e75dc54eb41f38ca6261c13708a2bf7dd
adamisrusty <adamgutierreztx@gmail.com>
2020-10-19 19:21:18 -0500

    initial commit

---
+++ library.cpp
@@ -0,0 +1,12 @@
+#include "library.h"
+
+#include <iostream>
+
+void menuGenerator(const std::string& menuTitle) {
+
+    std::cout << menuTitle << std::endl;
+}
+
+
+// TODO: implement menu colors
+// TODO: implement menu options with selectors
